21af04ce500ed9efa6141a36f7e9d2dc3ad4d1a3
Rob Eroh <rob@eroh.me>
2011-02-21 17:05:56 -0800

    the file was one less folder up"

--- Theme/handcrafted/core/style.php
+++ Theme/handcrafted/core/style.php
@@ -1,4 +1,4 @@
-<?php require_once('../../../../wp-load.php'); header('Content-Type: text/css'); global $d, $_der;
+<?php require_once('../../../wp-load.php'); header('Content-Type: text/css'); global $d, $_der;
 
 if ( defined('DER_STYLE_HEADER') ) { echo DER_STYLE_HEADER . "\n\n"; } else { echo "/* Dynamic CSS Code */\n\n"; }
 
